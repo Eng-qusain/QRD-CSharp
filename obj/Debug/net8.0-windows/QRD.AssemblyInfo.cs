dbcf480f0c78482534557cfc97bb595180328183
Eng-qusain <kossai1@live.com>
2026-06-29 12:56:36 +0200

    fff

--- obj/Debug/net8.0-windows/QRD.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/QRD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QRD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2166c1a6f4b9ea51d54e59376552cab6ae356b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924ec1aec65efc3e10b3a8fc7846b264ed36f0ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("QRD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QRD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
